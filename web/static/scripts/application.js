b9245b7d55cde0afc5e091b53c928e44921f463a
Void Zone <developer+github@voidzone.pl>
2015-11-02 23:14:17 +0100

    Easing

--- web/static/scripts/application.js
+++ web/static/scripts/application.js
@@ -6,7 +6,7 @@ $( document ).ready(function()
     $('li').click(function(e) {
         e.preventDefault();
         //$('#content').css('width', '50%');
-        TweenMax.to('#content, #background', 0.7, {width: '50%'});
+        TweenMax.to('#content, #background', 0.7, {width: '50%', ease: Power3.easeIn});
         //TweenMax.to('#background', 0.7, {width: '50%'});
         //$('#background').css('width', '50%');
     });
