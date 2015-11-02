5ef2607b988e5b4fa2c2ee355380f4a42ccf115f
Void Zone <developer+github@voidzone.pl>
2015-11-02 23:07:14 +0100

    Js and stuff

--- web/static/scripts/application.js
+++ web/static/scripts/application.js
@@ -5,7 +5,9 @@ $( document ).ready(function()
 {
     $('li').click(function(e) {
         e.preventDefault();
-        $('#content').css('width', '50%');
-        $('#background').css('width', '50%');
+        //$('#content').css('width', '50%');
+        TweenMax.to('#content', 0.7, {width: '50%'});
+        TweenMax.to('#background', 0.7, {width: '50%'});
+        //$('#background').css('width', '50%');
     });
 });
