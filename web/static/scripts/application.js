71ff311f975839394674b442d4b36cc6d40104fc
Void Zone <developer+github@voidzone.pl>
2015-11-02 23:44:51 +0100

    It seems that CSS is faster this time :)

--- web/static/scripts/application.js
+++ web/static/scripts/application.js
@@ -5,8 +5,8 @@ $( document ).ready(function()
 {
     $('li').click(function(e) {
         e.preventDefault();
-        $('#content').css('width', '50%');
         $('#background').css('width', '50%');
+        $('#content').css('width', '50%');
         //var tl = new TimelineLite();
         //tl.to('#background', 0.7, {width: '50%', ease: Power3.easeIn});
         //tl.to('#content', 0.7, {width: '50%', ease: Power3.easeIn});
