4daa3e947f03272bca220029187cacce1c1fc5b7
Void Zone <developer+github@voidzone.pl>
2015-11-02 23:09:36 +0100

    TweenMax

--- web/static/scripts/application.js
+++ web/static/scripts/application.js
@@ -6,8 +6,8 @@ $( document ).ready(function()
     $('li').click(function(e) {
         e.preventDefault();
         //$('#content').css('width', '50%');
-        TweenMax.to('#content', 0.7, {width: '50%'});
-        TweenMax.to('#background', 0.7, {width: '50%'});
+        TweenMax.to('#content, #background', 0.7, {width: '50%'});
+        //TweenMax.to('#background', 0.7, {width: '50%'});
         //$('#background').css('width', '50%');
     });
 });
